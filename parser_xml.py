0e5c38bbdf6f683aba4a12023f19a9ae173cdf3c
mona <120685754@qq.com>
2016-12-22 21:56:58 +0800

    parser json and xml

---
+++ parser_xml.py
@@ -0,0 +1,9 @@
+import xml.etree.ElementTree as et
+tree = et.ElementTree(file = '/home/mona/Documents/Python/learnpython/menu.xml')
+root = tree.getroot()
+print root.tag
+
+for child in root:
+	print('tag:', child.tag, 'attributes:', child.attrib)
+	for grandchild in child:
+		print('\ttag:', grandchild.tag, 'attributes:', grandchild.attrib)
